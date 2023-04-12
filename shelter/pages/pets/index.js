837853f93d33505a0a4977c4a7b41f9b313d0d33
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-04-12 22:32:40 +0200

    feat: add popup to our friends page

--- shelter/pages/pets/index.js
+++ shelter/pages/pets/index.js
@@ -74,7 +74,7 @@ fetch('../../pages/json/pets.json')
             }
         }
         generateNewPets();
-        console.log(generateNewPets())
+        // console.log(generateNewPets())
 
         const showPetsPage = () => {
             if (pageSize === 'desktop') {
@@ -93,23 +93,96 @@ fetch('../../pages/json/pets.json')
                     const pet = petsJson[i];
                     const petCard = document.createElement('div');
                     petCard.classList.add('slider__item');
-                    petCard.setAttribute('data-petNr', `${petsJson.indexOf(pet) + 1}`);
+                    petCard.setAttribute('data-petnr', `${petsJson.indexOf(pet) + 1}`);
                     petCard.innerHTML = `
-                        <div class="slider__item-img">
-                            <img class="item__image" src="../../assets/images/${pet.name}.png" alt="${pet.name}">
+                        <div class="slider__item-img" data-petnr="${petsJson.indexOf(pet) + 1}">
+                            <img class="item__image" src="../../assets/images/${pet.name}.png" alt="${pet.name}" data-petnr="${petsJson.indexOf(pet) + 1}">
                         </div>
-                        <div class="slider__item-name">${pet.name}</div>
+                        <div class="slider__item-name" data-petnr="${petsJson.indexOf(pet) + 1}">${pet.name}</div>
                         <button class="learn-more__button">Learn more</button>
                     `;
                     friendsGrid.appendChild(petCard);
-                    console.log(pet)
+                    // console.log(pet)
                 }
             }
+            friendsGrid.addEventListener('click', (e) => {
+                if (e.target.classList.contains('slider__item') 
+                || e.target.parentNode.classList.contains('slider__item') 
+                || e.target.parentNode.classList.contains('slider__item-img')) {
+                    // const targetPet = e.target.parentNode.getAttribute('data-petnr');
+                    // console.log(e.target)
+                    const targetPet = e.target.closest('div').dataset.petnr;
+        
+                    const currentPet = petsJson.find((pet) => {
+                        return pet.id === targetPet;
+                    });
+                    console.log(currentPet);
+    
+                            
+                    popup = document.createElement('div');
+                    popup.classList.add('popup__wrapper');
+                    popup.classList.add('popup__wrapper-open');
+                    document.body.classList.add('noscroll');
+                    
+                    const body = document.querySelector('body');
+                    body.appendChild(popup);
+                    popup.innerHTML = `
+                        <div class="popup__overlay">
+                            <div class="popup__window">
+                                <div class="popup__img">
+                                    <img src="../../assets/images/${currentPet.name}.png" alt="${currentPet.name}">
+                                </div>
+                                <div class="popup__content">
+                                    <div class="popup__title">${currentPet.name}</div>
+                                    <div class="popup__subtitle">${currentPet.type} - ${currentPet.breed}</div>
+                                    <p class="popup__text">${currentPet.description}</p>
+                                    <ul class="popup__list">
+                                        <li class="popup__item">Age: <span class="popup__age">${currentPet.age}</span></li>
+                                        <li class="popup__item">Inoculations: <span class="popup__inoculations">${currentPet.inoculations.join(', ')}</span></li>
+                                        <li class="popup__item">Diseases: <span class="popup__diseases">${currentPet.diseases.join(', ')}</span></li>
+                                        <li class="popup__item">Parasites: <span class="popup__parasites">${currentPet.parasites.join(', ')}</span></li>
+                                    </ul>
+                                </div>
+                                <button class="popup__close"><img src="../../assets/icons/close.svg" alt="close"></button>
+                            </div>
+                        </div>
+                    `; 
+                
+                popup.addEventListener('click', (e) => {
+                    if (e.target.classList.contains('popup__close') || e.target.classList.contains('popup__overlay') || e.target.parentNode.classList.contains('popup__close')) {
+                        console.log(e.target)
+                        document.body.classList.remove('noscroll');
+                        popup.style.visibility = 'hidden';
+                        popup.style.opacity = '0';
+                        popup.classList.remove('popup__wrapper-open');
+                        // popup.classList.remove('popup__wrapper');
+                        setTimeout(() => {
+                            popup.remove();
+                        }, 500)
+                    }
+                })
+                }
+            })
         }
-        showPetsPage()
-        console.log(petsJson)
+        showPetsPage();
+
+
+        nextBtn.addEventListener('click', () => {
+            currentPage++;
+            pageNrBtn.textContent = currentPage;
+            friendsGrid.remove(friendsGrid.firstChild);
+            showPetsPage();
+
+        })
+
+
+        
+
+
+
+
+        // console.log(petsJson)
     //     const currentPet = petsJson.find(el => el.name === e.target.closest('div').dataset.name);
     //     console.log(currentPet)
     
      })
-    

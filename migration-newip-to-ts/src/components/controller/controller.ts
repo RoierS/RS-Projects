d4610d69276c7de8b04864db04135655bd9c9531
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-06-11 12:30:05 +0200

    feat: migrate controller.js -> controller.ts

--- migration-newip-to-ts/src/components/controller/controller.ts
+++ migration-newip-to-ts/src/components/controller/controller.ts
@@ -1,7 +1,9 @@
 import AppLoader from "./appLoader";
 
-class AppController extends AppLoader {
-  getSources(callback) {
+type LoaderResCallBack<T> = (data: T) => void;
+
+class AppController<T> extends AppLoader {
+  getSources(callback: LoaderResCallBack<T>) {
     super.getResp(
       {
         endpoint: "sources",
@@ -10,20 +12,20 @@ class AppController extends AppLoader {
     );
   }
 
-  getNews(e, callback) {
-    let target = e.target;
-    const newsContainer = e.currentTarget;
+  getNews(e: Event, callback: LoaderResCallBack<T>) {
+    let target = e.target as HTMLElement;
+    const newsContainer = e.currentTarget as HTMLElement;
 
     while (target !== newsContainer) {
       if (target.classList.contains("source__item")) {
         const sourceId = target.getAttribute("data-source-id");
         if (newsContainer.getAttribute("data-source") !== sourceId) {
-          newsContainer.setAttribute("data-source", sourceId);
+          newsContainer.setAttribute("data-source", sourceId || "");
           super.getResp(
             {
               endpoint: "everything",
               options: {
-                sources: sourceId,
+                sources: sourceId || "",
               },
             },
             callback
@@ -31,7 +33,7 @@ class AppController extends AppLoader {
         }
         return;
       }
-      target = target.parentNode;
+      target = target.parentNode as HTMLElement;
     }
   }
 }

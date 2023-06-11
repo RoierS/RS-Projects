5798a1b3262f55440a9495d4e8907f08b88f901a
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-06-11 07:29:49 +0200

    feat: add api key update dependencies

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '437ed32951354fc0b63df437b21af3d9', // получите свой ключ https://newsapi.org/
         });
     }
 }

a127fdb957221bfa548fbb0e90ba4839def02909
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-06-11 11:23:30 +0200

    feat: migrate sources.js -> sources.ts

---
+++ migration-newip-to-ts/src/components/view/sources/sources.ts
@@ -0,0 +1,57 @@
+import "./sources.css";
+
+// enum ElementType {
+//   SourceItem = "source__item",
+//   SourceItemName = "source__item-name",
+// }
+
+interface Source {
+  name: string;
+  id: string;
+}
+
+class Sources {
+  private fragment: DocumentFragment;
+  private sourceItemTemp: HTMLTemplateElement | null;
+
+  constructor() {
+    this.fragment = document.createDocumentFragment();
+    this.sourceItemTemp = document.querySelector<HTMLTemplateElement>(
+      "#sourceItemTemp"
+    );
+  }
+
+  public draw(data: Source[]): void {
+    data.forEach((item) => {
+      if (!this.sourceItemTemp) {
+        return;
+      }
+      const sourceClone = this.sourceItemTemp.content?.cloneNode(
+        true
+      ) as DocumentFragment;
+
+      const sourceItemName = sourceClone.querySelector<HTMLElement>(
+        ".source__item-name"
+      );
+      if (sourceItemName) {
+        sourceItemName.textContent = item.name;
+      }
+
+      const sourceItem = sourceClone.querySelector<HTMLElement>(
+        ".source__item"
+      );
+      if (sourceItem) {
+        sourceItem.setAttribute("data-source-id", item.id);
+      }
+
+      this.fragment.append(sourceClone);
+    });
+
+    const sourceContainer = document.querySelector(".sources");
+    if (sourceContainer) {
+      sourceContainer.append(this.fragment);
+    }
+  }
+}
+
+export default Sources;

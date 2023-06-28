cff4418b28c68275fa5ef79c48d45a7e44ce4faa
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-06-28 13:46:00 +0200

    feat: add test class Game to build project

--- RSS-CSS-Selectors/src/index.ts
+++ RSS-CSS-Selectors/src/index.ts
@@ -1,2 +1,21 @@
-const msg = "Hi";
-console.log(msg);
+class Game {
+  private currentLevel: number;
+
+  constructor() {
+    this.currentLevel = 1;
+  }
+
+  startGame() {
+    this.displayLevel();
+  }
+
+  displayLevel() {
+    const levelElement = document.createElement("h1");
+    levelElement.textContent = `Level ${this.currentLevel}`;
+    document.getElementById("app")?.appendChild(levelElement);
+  }
+}
+
+const game = new Game();
+game.startGame();
+ 

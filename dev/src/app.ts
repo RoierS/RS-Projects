1d18e42c7569663d89f4523da1642fd60bb40e48
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-07-24 20:56:18 +0200

    chore: add alert

--- dev/src/app.ts
+++ dev/src/app.ts
@@ -2,6 +2,9 @@ import "./style.css";
 import Garage from "./views/garage/garage";
 import Header from "./views/header/header";
 
+// eslint-disable-next-line no-alert
+alert("Please review this work at last cross-check date");
+
 const header = new Header();
 header.render();
 
